aa195e665877914d5d86ad2d5b237eafd81640cb
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-09-07 13:29:14 +0530

    [Devesh]: ability to add hotel in program

--- Hotel-Reservation-System/lib/src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
+++ Hotel-Reservation-System/lib/src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
@@ -1,5 +1,24 @@
 package com.bridgelabz.hotelreservation;
+import java.util.ArrayList;
+
 
 public class HotelReservation {
-
+	String name;
+	Integer rating;
+	CustomerType customerType;
+	Integer weekdayRates;
+	Integer weekendRates;
+	
+	
+	public HotelReservation(String name, Integer rating, CustomerType customerType, Integer weekdayRates,
+			Integer weekendRates) {
+		super();
+		this.name = name;
+		this.rating = rating;
+		this.customerType = customerType;
+		this.weekdayRates = weekdayRates;
+		this.weekendRates = weekendRates;
+	}
+	
+	
 }
--- Hotel-Reservation-System/lib/src/test/java/com/bridgelabz/hotelreservation/HotelReservationMain.java
+++ Hotel-Reservation-System/lib/src/test/java/com/bridgelabz/hotelreservation/HotelReservationMain.java
@@ -1,9 +1,28 @@
 package com.bridgelabz.hotelreservation;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
+
 public class HotelReservationMain {
-
-	public static void main(String[] args) {
-
+	public ArrayList<HotelReservation> data = new ArrayList<HotelReservation>();
+	
+	@Before
+	public void addHotels() {
+		HotelReservation lakewoodRegular = new HotelReservation("lakewood",3,CustomerType.REGULAR,110,90);
+		HotelReservation lakewoodReward = new HotelReservation("lakewood",3,CustomerType.REWARDS,80,80);
+		
+		HotelReservation bridgewoodRegular = new HotelReservation("bridgewood",4,CustomerType.REGULAR,160,60);
+		HotelReservation bridgewoodReward = new HotelReservation("bridgewood",4,CustomerType.REWARDS,110,50);
+		
+		HotelReservation ridgewoodRegular = new HotelReservation("ridgewood",3,CustomerType.REGULAR,220,150);
+		HotelReservation ridgewoodReward = new HotelReservation("ridgewood",3,CustomerType.REWARDS,100,40);
+		
+		data.add(lakewoodRegular);
+		data.add(lakewoodReward);
+		data.add(bridgewoodRegular);
+		data.add(bridgewoodReward);
+		data.add(ridgewoodRegular);
+		data.add(ridgewoodReward);
 	}
-
 }
